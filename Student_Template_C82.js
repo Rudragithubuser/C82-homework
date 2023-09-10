8b9838f3a797b487f970b42fbdb018070d71b026
Rudragithubuser <139032341+Rudragithubuser@users.noreply.github.com>
2023-09-10 16:28:17 +0530

    Add files via upload

---
+++ Student_Template_C82.js
@@ -0,0 +1,46 @@
+console.log("My first console project");
+
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+
+/*
+Concatenate the first name and the last name to print the full name of the person on the console. 
+
+Use the "concat" method on the first_name variable.
+*/
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+/*
+Create a variable named add and store the addition value of a and b in that variable.
+
+Use the + operator to add a and b.
+*/
+var add = a + b;
+console.log(add);
+
+/*
+Create a variable named sub and store the subtraction value of a and b in that variable.
+
+Use the - operator to subtract b from a.
+*/
+var sub = a - b;
+console.log(sub);
+
+/*
+Create a variable named multiple and store the multiplication value of a and b in that variable.
+
+Use the * operator to multiply a and b.
+*/
+var multiple = a * b;
+console.log(multiple);
+
+/*
+Create a variable named divide and store the division value of a and b in that variable.
+
+Use the / operator to divide a by b.
+*/
+var divide = a / b;
+console.log(divide);
